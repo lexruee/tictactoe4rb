b1bbe759b8f4ed2a5e63cc610f55afd0189a38c4
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2014-12-29 00:54:02 +0100

    add tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,7 @@ module Tictactoe4rb
     context "#initialize" do
       it 'create board' do
         board = Board.new(3, 3)
+        expect(board.full?).to eq false
         expect(board.rows).to eq 3
         expect(board.columns).to eq 3
         3.times do |row|
@@ -14,6 +15,23 @@ module Tictactoe4rb
           end
         end
       end
+
+      it "create full board" do
+        board = Board.new(3, 3)
+        board[0,0] = :x
+        board[1,0] = :x
+        board[2,0] = :x
+
+        board[0,1] = :x
+        board[1,1] = :x
+        board[2,1] = :x
+
+        board[0,2] = :x
+        board[1,2] = :x
+        board[2,2] = :x
+        expect(board.full?).to eq true
+      end
+
     end
   end
 end
